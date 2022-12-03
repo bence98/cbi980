24082b22da8feedbcc502d588999b1ad137f6b55
Bence Cs <bence.csokas@gmail.com>
2022-12-03 21:02:05 +0100

    cbi980_core: Overflow and underflow signalling

--- cbi980_core.v
+++ cbi980_core.v
@@ -133,40 +133,54 @@ wire [23:0] aud_dout;
 reg  [23:0] aud_din0, aud_din1;
 
 always @(posedge clk)
-	if(rst) begin
-		rx_ovf[1] <= 1'b0;
+	if(rst)
 		r1head <= 'b0;
-	end else if(aud_dout_vld[1] & rx_en[1]) begin
+	else if(aud_dout_vld[1] & rx_en[1]) begin
 		r1fifo[r1head] <= aud_dout;
 		r1head <= r1head + 1;
 	end
 
 always @(posedge clk)
-	if(rst) begin
-		tx_unf[1] <= 1'b0;
+	if(rst)
 		t1tail <= 'b0;
-	end else if(aud_din_ack[1] & tx_en[1]) begin
+	else if(aud_din_ack[1] & tx_en[1]) begin
 		aud_din1 <= t1fifo[t1tail];
 		t1tail <= t1tail + 1;
 	end
 
 always @(posedge clk)
-	if(rst) begin
-		rx_ovf[0] <= 1'b0;
+	if(rst)
 		r0head <= 'b0;
-	end else if(aud_dout_vld[0] & rx_en[0]) begin
+	else if(aud_dout_vld[0] & rx_en[0]) begin
 		r0fifo[r0head] <= aud_dout;
 		r0head <= r0head + 1;
 	end
 
 always @(posedge clk)
-	if(rst) begin
-		tx_unf[0] <= 1'b0;
+	if(rst)
 		t0tail <= 'b0;
-	end else if(aud_din_ack[0] & tx_en[0]) begin
+	else if(aud_din_ack[0] & tx_en[0]) begin
 		aud_din0 <= t0fifo[t0tail];
 		t0tail <= t0tail + 1;
 	end
+
+genvar i;
+generate for(i=0; i<2; i=i+1) begin
+always @(posedge clk)
+	if(rst | irq_rst)
+		rx_ovf[i] <= 1'b0;
+	else if(aud_dout_vld[i] & rx_en[i] & rxf[i]) begin
+		rx_ovf[i] <= 1'b1;
+	end
+
+always @(posedge clk)
+	if(rst | irq_rst)
+		tx_unf[i] <= 1'b0;
+	else if(aud_din_ack[i] & tx_en[i] & txe[i]) begin
+		tx_unf[i] <= 1'b1;
+	end
+end
+endgenerate
 
 codec_if i2s_if(
 	.clk(clk),
